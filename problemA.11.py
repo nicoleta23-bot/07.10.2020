a2be85c4e808eddb759a802a19ad0271a83931a9
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2020-10-07 23:40:36 +0300

    Add files via upload

---
+++ problemA.11.py
@@ -0,0 +1,5 @@
+a=int(input("dati numarul iepurilor la inceputul lunii"))
+b=int(input("dati numarul de iepuri decedati"))
+c=int(input("dati numarul de iepuri care s-au nascut"))
+n=a-b+c 
+print("la sfirsitul lunii sunt",n,"iepuri")
