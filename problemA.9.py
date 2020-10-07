987772175455f10a6c106bfedbf32a92350257a3
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2020-10-07 23:52:38 +0300

    Add files via upload

---
+++ problemA.9.py
@@ -0,0 +1,9 @@
+a=int(input("dati primul numar"))
+b=int(input("dati al doilea numar"))
+c=int(input("dati al treilea numar"))
+s1=a+b
+s2=a+c
+s3=b+c   
+print(a,"+",b,"=",a+b)
+print(a,"+",c,"=",a+c)
+print(b,"+",c,"=",b+c)
